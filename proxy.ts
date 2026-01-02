3b5adfcbee3e36cf612e637d8a1f107f7525864c
Nischal Singana <singananischal@gmail.com>
2026-01-02 17:14:20 +0530

    updated proxy

--- proxy.ts
+++ proxy.ts
@@ -1,31 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import {getSessionCookie} from "better-auth/cookies"
+import { getSessionCookie } from "better-auth/cookies"
 
-const protectedRoutes=["/profile","/admin/dashboard"]
-const authRoutes=["/login", "/signup"]
+const protectedRoutes = ["/profile", "/admin/dashboard"]
+const authRoutes = ["/login", "/signup"]
 
-export async function proxy(req:NextRequest){
-    const {nextUrl}=req;
-    const sessionCookie =getSessionCookie(req)
-    const res=NextResponse.next()
+export async function proxy(req: NextRequest) {
+  const { nextUrl } = req;
+  const sessionCookie = getSessionCookie(req)
+  const res = NextResponse.next()
 
-    const isLoggedIn=!!sessionCookie
+  const isLoggedIn = !!sessionCookie
 
-    const isOnProtectedRoute=protectedRoutes.includes(nextUrl.pathname)
-    const isOnAuthRoute=authRoutes.includes(nextUrl.pathname)
+  const isOnProtectedRoute = protectedRoutes.includes(nextUrl.pathname)
+  const isOnAuthRoute = authRoutes.includes(nextUrl.pathname)
 
-    // If logged in and trying to access auth pages, redirect to profile
-    if(isLoggedIn && isOnAuthRoute){
-        return NextResponse.redirect(new URL("/profile",nextUrl))
-    }
+  // If logged in and trying to access auth pages, redirect 
+  if (isLoggedIn && isOnAuthRoute) {
+    return NextResponse.redirect(new URL("/profile", nextUrl))
+  }
 
-    // If not logged in and trying to access protected route, redirect to login
-    if(!isLoggedIn && isOnProtectedRoute){
-        return NextResponse.redirect(new URL("/login",nextUrl))
-    }
-   
-    return res
+  // If not logged in and trying to access protected route, redirect to login
+  if (!isLoggedIn && isOnProtectedRoute) {
+    return NextResponse.redirect(new URL("/login", nextUrl))
+  }
+
+  return res
 }
 
 export const config = {
@@ -33,4 +33,4 @@ export const config = {
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 }
-    
+
