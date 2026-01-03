556104e9e67b657f4d844ab9b91c4e8ec01fdac8
Nischal Singana <singananischal@gmail.com>
2026-01-03 12:43:24 +0530

    Optimized prisma connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,27 @@ import { PrismaClient } from "@/lib/generated/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+const prismaClientSingleton = () => {
+  let adapterOptions = {};
+
+  if (process.env.NODE_ENV === "development" && process.env.DATABASE_URL) {
+    let url = process.env.DATABASE_URL;
+    if (!url.includes("connection_limit")) {
+      url += `${url.includes("?") ? "&" : "?"}connection_limit=5&pool_timeout=20`;
+      adapterOptions = {
+        datasources: {
+          db: {
+            url
+          }
+        }
+      };
+      console.log("Applying dev DB connection limits");
+    }
+  }
+
+  return new PrismaClient(adapterOptions);
+};
+
+export const prisma = globalForPrisma.prisma || prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
