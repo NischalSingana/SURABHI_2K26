f7782763c898259d0a5b2e881e22a6f2c50b07b8
Nischal Singana <singananischal@gmail.com>
2026-01-11 01:07:56 +0530

    chore: remove unused prisma artifacts and scripts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
-
-import { PrismaClient } from "@/lib/generated/prisma";
+// HMR Trigger
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
